4cc042a79be7d1c6d1d445f76eae85350a7676ac
Hugo <hugodrion2@gmail.com>
2025-11-01 21:38:45 +0100

    debut normalisation du texte

--- projet.py
+++ projet.py
@@ -5,13 +5,22 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("cannabis_recreatif.csv", encoding="latin1", sep=';')
 
-print(df.head)
+# print(df.head)
 import spacy
 
 # chargement du modele spaCy pour la langue francaise
 nlp = spacy.load("fr_core_news_sm")
 
-doc = nlp(input_text)
+# fonction pour lemmatiser le texte
+def lemmatize_text(text):
+    if pd.isna(text):
+        return text
+    doc = nlp(text)
+    return " ".join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
 
-# extraction des mots lemmatises
+df["texte_lemmatise"] = df["Vous pouvez préciser votre réponse."].apply(lemmatize_text)
+df.to_csv("cannabis_recreatif_lemmatise.csv", index=False, sep=';', encoding='utf-8')
+# exemple sur une chaine simple (gardé pour référence)
+doc = nlp("Les étudiants apprennent le traitement automatique du langage naturel avec spaCy.")
 lemmatized_text = " ".join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
+#print(lemmatized_text)
