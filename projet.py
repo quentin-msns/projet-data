71920d4340bac38df0261d78275a59130fb7d88a
Hugo <hugodrion2@gmail.com>
2025-11-02 20:38:16 +0100

    trql

--- projet.py
+++ projet.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("cannabis_recreatif.csv", encoding="latin1", sep=';')
-
+df1 = df.head(10).copy()
 # print(df.head)
 import spacy
 
@@ -18,9 +18,10 @@ def lemmatize_text(text):
     doc = nlp(text)
     return " ".join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
 
-df["texte_lemmatise"] = df["Vous pouvez préciser votre réponse."].apply(lemmatize_text)
-df.to_csv("cannabis_recreatif_lemmatise.csv", index=False, sep=';', encoding='utf-8')
+df1["texte_lemmatise"] = df1["Vous pouvez préciser votre réponse."].apply(lemmatize_text)
+df1.to_csv("cannabis_recreatif_lemmatise_10.csv", index=False, sep=';', encoding='utf-8')
 # exemple sur une chaine simple (gardé pour référence)
-doc = nlp("Les étudiants apprennent le traitement automatique du langage naturel avec spaCy.")
-lemmatized_text = " ".join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
-#print(lemmatized_text)
+#print(df.columns)
+print(df1.columns.tolist())
+
+print(df1[["Vous pouvez préciser votre réponse.", 'Vous pouvez préciser votre réponse..1','Quel(s) autre(s) avantage(s) verriez-vous à lassouplissement de la politique actuelle ?', 'Selon vous y aurait-il une ou plusieurs autres priorités budgétaires ?','Pour quelle(s) raison(s) ?','Y a-t-il une ou des raisons supplémentaires pour lesquelles vous vous opposez à sa dépénalisation ou sa légalisation ?','Vous pouvez déposer ici une contribution écrite. ']])
